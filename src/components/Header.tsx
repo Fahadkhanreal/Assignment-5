8c6479d7ee522a63a4f00dc56846cd241026e7c0
Fahadkhanreal <fhadikhan00@gmail.com>
2024-11-05 14:57:27 +0500

    Assignment-5

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='h-[134px] bg-[#A29875] text-[#ffff] text-[75px] pl-5 '>
+      MANZZARI
+    </div>
+  )
+}
+
+export default Header
